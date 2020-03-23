5fceea63a2f1e2f261d412d4daf8b3b8747eb440
tigerabrodi <fahdabrodi@gmail.com>
2020-03-23 12:49:48 +0100

    first commit, app is finished. Very basic jest and enzyme, learning Testing and TDD.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-    
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      counter: 0
+    }
+  }
+  render() {
+      return (
+    <div data-test="component-app">
+      <h1 data-test="counter-display">The counter is currently {this.state.counter} </h1>
+      <button data-test="increment-button"
+      onClick={() => this.setState({counter: this.state.counter + 1})}
+      >Increment counter</button>
+      <button data-test="decrement-button"
+      onClick={() => this.setState({counter: this.state.counter - 1})}>Decrement Button</button>
     </div>
   );
+  }
 }
 
 export default App;
